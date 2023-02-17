837370c5a18c8908de8a82ad0d3812bb10f3a37f
Evanderson <92355749+Evapereira2398@users.noreply.github.com>
2023-02-17 19:10:08 -0300

    Atu project

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,8 @@
+import java.net.SocketPermission;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Realizando o upload do projeto no GitHub");
+        System.out.println("Projeto sendo criado pelo VScode e sendo sincronizado no GitHub")
     }
 }
